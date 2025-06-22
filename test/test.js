d13f0e202db759b098af5e28abb1805b0c8f6d25
ahamedajaj01 <ahamed.ajaj01@gmail.com>
2025-06-23 00:12:49 +0545

    Initial commit

---
+++ test/test.js
@@ -0,0 +1,10 @@
+test("example test", ()=>{
+    expect( 1 +1).toBe(2);
+})
+
+test("example test", ()=>{
+    const str = "hello";
+ const reverse = str.split("").reverse().join("")
+expect(reverse).toBe("olleh")
+
+})
